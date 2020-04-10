a139eb870dc815fb82ec0575ddc470d66967a135
Walter <lalala19@126.com>
2020-04-10 18:12:45 +0800

    c

--- src/main/java/com/demo/gateway/gateway/GatewayApplication.java
+++ src/main/java/com/demo/gateway/gateway/GatewayApplication.java
@@ -1,26 +1,41 @@
 package com.demo.gateway.gateway;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
  
 @EnableDiscoveryClient
+@EnableEurekaClient
 @SpringBootApplication
+@Configuration
 public class GatewayApplication {
 	
 
+
 	
-	@Bean
-    public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
-        return builder.routes().route(r -> r.path("/test/baidu").uri("http://ww.baidu.com"))
-                .route(r -> r.path("/mweb/**").uri("lb://mweb"))
-                .build();
-    }
+	
+//
+//	 @Bean
+//	    public RouteLocator gatewayRoutes(RouteLocatorBuilder builder) {
+//	        return builder.routes()
+//	                .route(r -> r.path("/mweb/**")
+//	                        .uri("lb://mweb-service")
+//	                        .id("mwebModule"))
+//	                
+//	                .build();
+//	    
+//
+//	}
+
  
 	public static void main(String[] args) {
+		System.out.print("1");
 		SpringApplication.run(GatewayApplication.class, args);
 	}
 
